8a58deaf9a9b760f0423aea8f738acc97f2df378
Youchan Kim <yckim@ucdavis.edu>
2019-05-01 16:57:53 -0700

    first button has onclick function

--- Uetility/Uetility/MainActivity.cs
+++ Uetility/Uetility/MainActivity.cs
@@ -18,8 +18,16 @@ namespace Uetility
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
-                                  
-        }        
-	}
+
+            Button btn1 = FindViewById<Button>(Resource.Id.button1);
+            btn1.Click += button1Click;
+        }
+
+        private void button1Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(ApplicationContext, "hello", ToastLength.Long).Show();
+        }
+        //[Android(ScreenOrientation = ScreenOrientation.Portrait)]
+    }
 }
 
